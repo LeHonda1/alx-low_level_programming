1a7677f3da504b46abedd1789fcdc8713e7ae9f7
LeHonda1 <Honda56444@example.com>
2023-02-20 10:05:24 -0800

    tasks

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+int main(void)
+{
+ 
+  int n =96;
+  while (n++<122)
+    putchar(n);
+  return 0;
+
+}    int i = 1;
+    while(i++ < 10)
+         printf("%d\n", i);
+
+    return 0;
