34247139a0c9d64e48ebd7890fe4e1d25c2f82d7
LeHonda1 <Honda56444@outlook.com>
2023-02-21 05:03:46 -0800

    tasks

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,21 +8,21 @@
  *         0 if the number is zero,
  *         -1 if the number is less than zero.
  */
-int print_sign(int n);
+int print_sign(int n)
 {
-	if (n>0)
+	if (n > 0)
 	{
-	    printf("+");
+		_putchar('+');
 		return (1);
-	}	
-	else if (n<0)
+	}
+	else if (n == 0)
 	{
-	   printf("-");
-	   return(-1);
-	}   
+		_putchar('0');
+		return (0);
+	}
 	else
 	{
-	    printf("0");
-		return (0);
-	}	
+		_putchar('-');
+		return (-1);
+	}
 }
