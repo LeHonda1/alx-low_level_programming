4ecc73fa2f71807961a17915415aede2ff819452
LeHonda1 <Honda56444@outlook.com>
2023-02-21 10:31:19 -0800

    tasks

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,13 +1,19 @@
-#include <stdio.h>
+#include "main.h"
 
-int print_last_digit(int num) {
-    int last_digit = num % 10;  // Get the remainder of num divided by 10
-    return last_digit;  // Return the value of the last digit
-}
+/**
+ * print_last_digit - Prints the last digit of a number.
+ * @n: The number in question.
+ *
+ * Return: Value of the last digit.
+ */
+int print_last_digit(int n)
+{
+	int last_digit = n % 10;
 
-int main() {
-    int num = 12345;
-    int last_digit = print_last_digit(num);
-    printf("The value of the last digit is %d\n", last_digit);
-    return 0;
+	if (last_digit < 0)
+		last_digit *= -1;
+
+	_putchar(last_digit + '0');
+
+	return (last_digit);
 }
