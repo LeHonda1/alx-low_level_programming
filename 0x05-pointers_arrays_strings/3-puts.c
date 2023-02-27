f0fbfd0bd75ed137ebc3c35a05b31d8da7c9e0b9
LeHonda1 <124697668+LeHonda1@users.noreply.github.com>
2023-02-27 16:06:44 +0200

    Update 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,17 +1,19 @@
-#include "main.h"
+#include"main.h"
 
 /**
- * _puts - prints a string to stdout, followed by a newline character
+ * _puts - prints a string, followed by a newline to stdout
  *
- * @str: pointer to a string
- */
+ * @str: string parameter to print
+ *
+ * Return: Nothing
+*/
+
 void _puts(char *str)
 {
-    while (*str != '\0') 
-    {
-        putchar(*str++);
-        str++;
-    }
-    
-    putchar('\n');
+	while (*str != '\0')
+	{
+		_putchar(*str + 0);
+		++str;
+	}
+	_putchar('\n');
 }
