df57906c19ad76e0882e2eaafccb9d249a27066f
LeHonda1 <Honda56444@outlook.com>
2023-02-27 04:15:16 -0800

    tasks

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,12 @@
+/**
+ * reset_to_98 - updates the value it points to to 98
+ *
+ * @n: pointer to an integer
+ *
+ * Return: void
+ */
 void reset_to_98(int *n)
- /**
-  * the function takes a pointer to an integer n as a parameter
-  *  the value it points to by dereferencing the pointer using the * operator and setting it to 98.
-  */
-
 {
-    *n = 98;
+	*n = 98;
 }
+
