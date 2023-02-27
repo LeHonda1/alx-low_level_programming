85a3b64d69e85113ffc38d4c2a07eafeb16a9019
LeHonda1 <Honda56444@outlook.com>
2023-02-27 04:51:48 -0800

    tasks

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -5,11 +5,15 @@
  *
  * Return: the length of the string
  */
-int _strlen(char *s){
-    int len=0; 
-    while(*s!='\0'){
-        len++;
-        s++;
-    }
-    return(len);
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+
+	return (len);
 }
