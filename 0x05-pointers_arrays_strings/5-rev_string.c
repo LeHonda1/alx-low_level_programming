4eedfc779320b94443e6257cedf3ca460925fd6e
LeHonda1 <124697668+LeHonda1@users.noreply.github.com>
2023-02-28 13:42:04 +0200

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,15 +8,17 @@ void rev_string(char *s)
 {
     int length = 0;
     int i;
+    char temp;
 
     while (s[length] != '\0')
     {
         length++;
     }
 
-    for ( i = length - 1; i >= 0; i--)
+    for ( i = 0; i < length/2; i++)
     {
-        printf("%c", s[i]);
+      temp=s[i];
+      s[i]=s[length-i-1];
+      s[length-i-1]=temp;
     }
 }
-
