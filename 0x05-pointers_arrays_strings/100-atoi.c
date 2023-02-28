754a555c51dace15f13ab763164d92eed9661209
LeHonda1 <124697668+LeHonda1@users.noreply.github.com>
2023-02-28 22:56:39 +0200

    Update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,9 +1,3 @@
-/**
- * _atoi - convert a string to an integer
- * @s: the string to convert
- *
- * Return: the integer value of the string, or 0 if the string does not contain any integers
- */
 int _atoi(char *s)
 {
     int sign = 1;
@@ -14,10 +8,9 @@ int _atoi(char *s)
             sign *= -1;
         }
         else if (*s >= '0' && *s <= '9') {
-            num = num * 10 + (*s - '0');
+            num = num *8 10 + (*s - '0');
         }
         else if (num > 0) {
-            // Stop parsing the string if we encounter a non-numeric character
             break;
         }
         s++;
