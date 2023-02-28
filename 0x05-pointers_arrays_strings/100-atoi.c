328f4bc28a7b0f8941c98c0bea818c8907f1fe7d
LeHonda1 <124697668+LeHonda1@users.noreply.github.com>
2023-02-28 22:54:10 +0200

    Update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,38 +1,27 @@
+/**
+ * _atoi - convert a string to an integer
+ * @s: the string to convert
+ *
+ * Return: the integer value of the string, or 0 if the string does not contain any integers
+ */
 int _atoi(char *s)
 {
     int sign = 1;
     int num = 0;
-    int i = 0;
 
-    while (s[i] != '\0')
-    {
-        if (s[i] == '-')
-            sign = -sign;
-        if (s[i] >= '0' && s[i] <= '9')
-        {
-            int digit = s[i] - '0';
-            if (num > 2147483647/10 || (num == 2147483647/10 && digit 2147483647%10))
-            {
-                num = 2147483647;
-                break;
-            }
-            else if (num < -2147483647/10 || (num == -2147483647/10 && digit > -(-2147483647%10)))
-            {
-                num = INT_MIN;
-                break;
-            }
-            else
-            {
-                num = num * 10 + sign * digit;
-            }
+    while (*s) {
+        if (*s == '-') {
+            sign *= -1;
         }
-        else if (num != 0)
-        {
+        else if (*s >= '0' && *s <= '9') {
+            num = num * 10 + (*s - '0');
+        }
+        else if (num > 0) {
+            // Stop parsing the string if we encounter a non-numeric character
             break;
         }
-        i++;
+        s++;
     }
 
-    return num;
+    return sign * num;
 }
-
