16ba4de2971bccc00f875700f264bd1472a81684
LeHonda1 <124697668+LeHonda1@users.noreply.github.com>
2023-02-28 13:21:04 +0200

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -4,18 +4,18 @@
  * rev_string - Reverses a string.
  * @str: Pointer to string to reverse.
  */
-void rev_string(char *str)
+void rev_string(char *s)
 {
     int length = 0;
 
-    while (str[length] != '\0')
+    while (s[length] != '\0')
     {
         length++;
     }
 
     for (int i = length - 1; i >= 0; i--)
     {
-        printf("%c", str[i]);
+        printf("%c", s[i]);
     }
 }
 
