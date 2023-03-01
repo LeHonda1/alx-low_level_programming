5b883bfc8a413edd3f13762bbfe89744c4eb9a9a
LeHonda1 <Honda56444@outlook.com>
2023-03-01 06:39:39 -0800

    task

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+char* _strcat(char *dest, char *src) {
+    int dest_len = strlen(dest);
+    int src_len = strlen(src);
+    char *ptr = dest + dest_len;
+    int i;
+    for ( i = 0; i < src_len; i++) {
+        *ptr++ = *src++;
+    }
+    *ptr = '\0';
+    return dest;
+}  
+
