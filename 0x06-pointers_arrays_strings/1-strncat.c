c573a33ecbcdc3c1b62f909da59c881e6b00f7fb
LeHonda1 <Honda56444@outlook.com>
2023-03-01 10:42:07 -0800

    task

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strcat - Concatenates two strings
+ * @dest: Destination string
+ 8* @src: Source string
+ * Return: Pointer to the resulting string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+    int dest_len = strlen(dest);
+    int src_len = strlen(src);
+    char *ptr = dest + dest_len;
+    int i;
+
+    for (i = 0; i < n; i++)
+    { 
+         *ptr++ = *src++;
+    }
+    *ptr = '\0';
+    return dest;
+}
