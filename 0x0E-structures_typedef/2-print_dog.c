93361d2dd49a7d00cdbd7b6ffe053de9694eb9bc
LeHonda1 <124697668+LeHonda1@users.noreply.github.com>
2023-03-20 18:55:06 +0200

    Update 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,13 +12,17 @@
  */
 void print_dog(struct dog *d)
 {
-    if (d != NULL) {
-        char name[10];
-        float age ;
-        char owner[10];
-        printf("Name: %s\n",(*d).name);
-        printf("Age: %f\n ",(*d).age);
-         printf("Owner: %s\n",(*d).owner);
-    }
+	if (d != NULL)
+	{
+		if (d->name != NULL)
+			printf("Name: %s\n", d->name);
+		else
+			printf("Name: (nil)\n");
+		printf("Age: %.6f\n", d->age);
+		if (d->owner != NULL)
+			printf("Owner: %s\n", d->owner);
+		else
+			printf("Owner: (nil)\n");
+	}
     
 }
